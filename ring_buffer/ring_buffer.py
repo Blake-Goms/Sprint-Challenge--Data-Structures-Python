091a7575f3ed102163b06f90a16a38f017099f4e
Blake Goms <blakegoms@gmail.com>
2020-01-31 10:51:13 -0800

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,33 +7,57 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+
     def append(self, item):
-        if self.capacity > self.storage.__len__():
-            self.storage.add_to_head(item)
+        if self.current == None:
+            self.current = 0 
+
+        current_head = self.storage.head
+        # if our head is None, move item to tail
+        if current_head is None:
+            self.storage.add_to_tail(item)
+            self.current += 1
+            return
         else:
-            self.storage.remove_from_tail()
-            self.storage.add_to_head(item)
+            # loop through current
+            for i in range(self.current):
+                # if next node/element is not None
+                if current_head.next != None:
+                    # then set that next node/element to current_head
+                    current_head = current_head.next
+                else:
+                    # if next node/element is None
+                    # put item at tail
+                    self.storage.add_to_tail(item)
+                    self.current += 1
+                    if self.current == self.capacity:
+                        self.current = 0
+                    return
+            current_head.value = item
+            self.current += 1
+            if self.current == self.capacity:
+                self.current = 0
 
     def get(self):
-        # Note:  This is the only [] allowed
+        #Note:  This is the only [] allowed
         list_buffer_contents = []
         temp = 0
-        while temp < self.storage.__len__():
-            selectedHead = self.storage.head
-            list_buffer_contents.append(selectedHead.value)
-            self.storage.move_to_front(selectedHead)
+        while temp < self.storage.__len__(): # loop through storage
+            current_head = self.storage.head # grab head
+            list_buffer_contents.append(current_head.value) # append head value to list
+            self.storage.move_to_end(current_head) # move head to tail
             temp += 1
         return list_buffer_contents
 
 
 
-# test = RingBuffer(4)
-# test.append('a')
-# test.append('b')
-# test.append('c')
-# test.append('d')
-# test.append('e')
-# print(test.get())
+#     def append(self, item):
+#         if self.capacity > self.storage.__len__():
+#             self.storage.add_to_head(item)
+#         else:
+#             self.storage.remove_from_tail()
+#             self.storage.add_to_head(item)
+
 
 
 
