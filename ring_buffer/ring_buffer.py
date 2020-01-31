52fa4e47485145602d73fc7b736d76e992190225
Blake Goms <blakegoms@gmail.com>
2020-01-31 09:12:47 -0800

    finished names and reverse

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,34 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.capacity > self.storage.__len__():
+            self.storage.add_to_head(item)
+        else:
+            self.storage.remove_from_tail()
+            self.storage.add_to_head(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
-
+        temp = 0
+        while temp < self.storage.__len__():
+            selectedHead = self.storage.head
+            list_buffer_contents.append(selectedHead.value)
+            self.storage.move_to_front(selectedHead)
+            temp += 1
         return list_buffer_contents
+
+
+
+# test = RingBuffer(4)
+# test.append('a')
+# test.append('b')
+# test.append('c')
+# test.append('d')
+# test.append('e')
+# print(test.get())
+
+
 
 # ----------------Stretch Goal-------------------
 
